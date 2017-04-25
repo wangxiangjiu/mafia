c2aeeaa76d73353e2be8ffa26e816e74bab42b18
William Tsay <willtsay@gmail.com>
2017-04-25 15:51:36 -0700

    added sockets, changed express a little bit

---
+++ index.js
@@ -0,0 +1,114 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(cors());
+app.use(express.static(__dirname));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/game.html');
+});
+
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+
+// FOR A ROOM. 
+// https://p7lrmho5n7.execute-api.us-east-1.amazonaws.com/prod/RecipeUpdate?TableName=mafia
+//  
+
+
+// get expected number of players
+
+// GFAME STATE LINK 
+
+// https://p7lrmho5n7.execute-api.us-east-1.amazonaws.com/prod/RecipeUpdate?TableName=Game_state 
+
+// what data structures do i want to use?
+
+var roles5 = ["Villager","Werewolf","Werewolf", "Seer", "Villager"];
+var roles7 = ["Villager","Villager","Villager","Werewolf", "Seer", "Doctor", "Werewolf"];
+var roles9 = ["Villager","Villager","Villager","Werewolf", "Seer", "Doctor", "Werewolf", "Villager", "Villager"];
+var roles11 = ["Villager","Villager","Villager","Werewolf", "Seer", "Doctor", "Werewolf", "Werewolf", "Villager", "Villager", "Villager"];
+
+var usernames = {};
+
+var roomcounts = {};
+
+var roomMax = {};
+
+var roomroles = {};
+
+var rooms = {};
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('join room', function(data){
+    // check if room is setup?
+    var room = data["room"];
+    var max = data["max"];
+
+    // hard coded, remove when role #'s dynamic;
+    max = 7;
+    
+    socket.join(room);
+    if (!rooms[room]){
+      rooms[room] = {};
+      rooms[room].pc = 1; 
+      rooms[room].max = max;
+      // later add logic for deciding how many roles
+      rooms[room].remainingRoles = roles7.slice();
+    } else {
+      rooms[room].pc +=1;
+    }
+    if (rooms[room].pc <= rooms[room].max){
+      var numRolesLeft = rooms[room].remainingRoles.length;
+      var index = Math.floor(Math.random()*numRolesLeft)
+      var role = rooms[room].remainingRoles[index];
+      rooms[room].remainingRoles.splice(index ,1);
+      console.log(role + "assigned to player");
+      console.log(JSON.stringify(rooms[room].remainingRoles) + "roles left to assign");
+    }
+
+    var username = usernames[socket.id]
+    socket.join(socket.id);
+    io.in(socket.id).emit("welcome", "WELCOME TO ROOM: "+ room + " " + username);
+    io.in(room).emit("roomcount" , "CURRENTLY " + rooms[room].pc +" IN ROOM " + room);
+    // send data back for client to post
+
+    var roleData = {};
+    roleData["Role"] = role;
+    roleData["GameId"] = room;
+    roleData["Player"] = rooms[room].pc+20;
+    
+
+
+
+    io.in(socket.id).emit("assign role", roleData); // tell client what role they got..  
+    // now roll the role?
+
+    // socket.to(room).emit("WELCOME TO ROOM: " + room);
+
+
+
+  });
+
+  socket.on('add user', function(username){
+    usernames[socket.id] = username;
+  });
+
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
---
+++ main.js
@@ -0,0 +1,145 @@
+// AWS.config.update({
+//   region:
+//   endpoint:
+//   accessKeyId:
+//   secretAccessKey: 
+// });
+
+// var dynamodb = new AWS.DyanomDB();
+// var docClient = new AWS.DyanomDB.DocumentClient();
+
+
+$(function() {
+  var FADE_TIME = 150; // ms
+  var TYPING_TIMER_LENGTH = 400; // ms
+
+  var $window = $(window);
+  var $usernameInput = $('.usernameInput'); // Input for username
+  var $roomInput = $('.roomInput');
+  var $loginPage = $('.login.page');
+  var $lobbyPage = $('.lobby.page');
+  var $gamePage = $('.game.page');
+  var $gameMessage = $('.game.message');
+  var $gameBroadcast = $('.game.broadcast');
+
+  var username = false;
+  var connected = false;
+  var $currentInput = $usernameInput.focus();
+
+  var socket = io();
+
+  socket.on("welcome", function(message){
+    $("div.welcome").text(message);
+    // set something to some message.
+  })
+
+  socket.on("roomcount", function(message){
+    $("div.broadcast").text(message);
+  })
+
+  socket.on("assign role", function(roleData){
+    // $.ajax({
+    //   type: "POST",
+    //   url: "https://p7lrmho5n7.execute-api.us-east-1.amazonaws.com/prod/RecipeUpdate?TableName=mafia",
+    //   data: role,
+
+    // })
+    var url = "https://d8iqr83w63.execute-api.us-east-1.amazonaws.com/prod/MafiaRequest";
+
+    console.log(roleData);
+    $.post(url, JSON.stringify({
+      "type":"POST",
+      "Access-Control-Allow-Origin": "*",
+      "data": {
+        "TableName" : "mafia",
+        "Item":{
+          "Player": roleData["Player"],
+          "Role": roleData["Role"]}}})
+    , function(data, status){
+      // alert(status);
+    });
+
+  })
+
+
+
+
+
+  function setUsername () {
+    username = cleanInput($usernameInput.val().trim());
+    // $loginPage.fadeOut();
+    // $lobbyPage.show();
+
+    // If the username is valid
+    if (username) {
+      $loginPage.fadeOut();
+      $lobbyPage.show();
+      $roomInput.focus();
+
+      socket.emit('add user', username);
+    //   // $loginPage.off('click');
+    //   // $currentInput = $inputMessage.focus();
+
+    //   // Tell the server your username
+    //   // socket.emit('add user', username);
+    }
+  }
+
+
+  socket.on()
+
+  function joinRoom () {
+    room = cleanInput($roomInput.val().trim());
+    if (room) {
+      $lobbyPage.fadeOut();
+      $gamePage.show();
+      $.get("https://d8iqr83w63.execute-api.us-east-1.amazonaws.com/prod/MafiaRequest?TableName=Game_state", function(data) {
+        // if room does not exist, tell player to reprompt alexa i think.
+        entries = data["Items"];
+        var data = false;
+        data = validRoom(entries);
+        if (data){
+          socket.emit("join room", data);
+        } else {
+          console.log("invalid room!");
+        }
+      });
+    }
+  }
+
+  function cleanInput (input) {
+    return $('<div/>').text(input).text();
+  }
+
+  function validRoom (items) {
+    var data = {};
+    for (var i=0; i<items.length; i++ ){
+      var entry = items[i];
+      if (entry["RoomId"]==room){
+        data["room"] = entry["RoomId"];
+        data["max"] = entry["Num_people"];
+        return data;
+      }
+    }
+    return false;
+  }
+
+  $window.keydown(function (event) {
+    // Auto-focus the current input when a key is typed
+    // if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+    //   $currentInput.focus();
+    // }
+    // When the client hits ENTER on their keyboard
+    if (event.which === 13) {
+      if (username){
+        joinRoom();
+      } else {
+  	    setUsername();      
+      }
+    }
+  });
+
+
+});
+
+
